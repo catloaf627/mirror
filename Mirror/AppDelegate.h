4ce08bfa00eb1d93e88b0e871ddcb150b3a3dc6b
wangyuqing.rachel <wangyuqing.rachel@gmail.com>
2022-09-24 22:22:55 +0800

    Initial Commit

---
+++ Mirror/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Mirror
+//
+//  Created by Yuqing Wang on 2022/9/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Mirror/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Mirror
+//
+//  Created by Yuqing Wang on 2022/9/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
