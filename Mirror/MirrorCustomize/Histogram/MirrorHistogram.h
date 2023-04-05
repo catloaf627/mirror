7e5dde1412ce3f9e4ade4f6fbacefac31a53f38a
wangyuqing.rachel <wangyuqing.rachel@gmail.com>
2023-04-05 18:40:53 +0800

    bar chart

---
+++ Mirror/MirrorCustomize/Histogram/MirrorHistogram.h
@@ -0,0 +1,16 @@
+//
+//  MirrorHistogram.h
+//  Mirror
+//
+//  Created by Yuqing Wang on 2023/4/5.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MirrorHistogram : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Mirror/MirrorHistory/HistogramView.m
+++ Mirror/MirrorHistory/HistogramView.m
@@ -7,6 +7,15 @@
 
 #import "HistogramView.h"
 #import "UIColor+MirrorColor.h"
+#import <Masonry/Masonry.h>
+#import "UIColor+MirrorColor.h"
+#import "MirrorHistogram.h"
+
+@interface HistogramView ()
+
+@property (nonatomic, strong) MirrorHistogram *barChart;
+
+@end
 
 @implementation HistogramView
 
@@ -14,15 +23,33 @@
 {
     self = [super init];
     if (self) {
-        self.backgroundColor = [UIColor mirrorColorNamed:MirrorColorTypeAddTaskCellBG];
+        self.backgroundColor = [UIColor mirrorColorNamed:MirrorColorTypeBackground];
         self.layer.cornerRadius = 14;
+        [self p_setupUI];
     }
     return self;
+}
+
+- (void)p_setupUI
+{
+    [self addSubview:self.barChart];
+    [self.barChart mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.top.bottom.offset(0);
+    }];
 }
 
 - (void)reloadHistogramView
 {
     
 }
+
+- (MirrorHistogram *)barChart
+{
+    if (!_barChart) {
+        _barChart = [MirrorHistogram new];
+    }
+    return _barChart;
+}
+
 
 @end
