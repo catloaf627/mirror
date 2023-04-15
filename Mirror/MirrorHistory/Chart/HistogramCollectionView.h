e22bc418e5640f651da676c0418577b621118e69
wangyuqing.rachel <wangyuqing.rachel@gmail.com>
2023-04-15 13:06:23 +0800

    files

---
+++ Mirror/MirrorHistory/Chart/HistogramCollectionView.h
@@ -0,0 +1,16 @@
+//
+//  HistogramCollectionView.h
+//  Mirror
+//
+//  Created by Yuqing Wang on 2023/4/14.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HistogramCollectionView : UICollectionView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Mirror/MirrorHistory/Chart/HistogramCollectionView.m
@@ -0,0 +1,20 @@
+//
+//  HistogramCollectionView.m
+//  Mirror
+//
+//  Created by Yuqing Wang on 2023/4/14.
+//
+
+#import "HistogramCollectionView.h"
+
+@implementation HistogramCollectionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
