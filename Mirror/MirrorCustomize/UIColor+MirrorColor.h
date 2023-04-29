9b9c56bac8eda567fe8eec1ddf50a8bea6dd66d9
wangyuqing.rachel <wangyuqing.rachel@gmail.com>
2023-04-29 11:59:18 +0800

    color enum

--- Mirror/MirrorCustomize/UIColor+MirrorColor.h
+++ Mirror/MirrorCustomize/UIColor+MirrorColor.h
@@ -15,6 +15,26 @@ typedef NS_ENUM (NSInteger, MirrorUserInterfaceStyle) {
 };
 
 typedef NS_ENUM(NSInteger, MirrorColorType) {
+    
+    MirrorColorTypeCellPink =  0,
+    MirrorColorTypeCellPinkPulse =  1,
+    MirrorColorTypeCellOrange = 2,
+    MirrorColorTypeCellOrangePulse = 3,
+    MirrorColorTypeCellYellow = 4,
+    MirrorColorTypeCellYellowPulse = 5,
+    MirrorColorTypeCellGreen = 6,
+    MirrorColorTypeCellGreenPulse = 7,
+    MirrorColorTypeCellTeal = 8,
+    MirrorColorTypeCellTealPulse = 9,
+    MirrorColorTypeCellBlue = 10,
+    MirrorColorTypeCellBluePulse = 11,
+    MirrorColorTypeCellPurple = 12,
+    MirrorColorTypeCellPurplePulse = 13,
+    MirrorColorTypeCellGray = 14,
+    MirrorColorTypeCellGrayPulse = 15,
+    
+    // 后续添加颜色从这里添加
+    
     MirrorColorTypeBackground,               // 主背景色
     MirrorColorTypeTabbarIconText,           // tabbar item 未选中的颜色
     MirrorColorTypeTabbarIconTextHightlight, // tabbar item 被选中的颜色
@@ -23,22 +43,6 @@ typedef NS_ENUM(NSInteger, MirrorColorType) {
     MirrorColorTypeShadow,                   // 阴影颜色
     MirrorColorTypeAddTaskCellBG,            // add task cell的背景颜色
     MirrorColorTypeAddTaskCellPlus,          // add task cell的加号颜色
-    MirrorColorTypeCellPink,
-    MirrorColorTypeCellPinkPulse,
-    MirrorColorTypeCellOrange,
-    MirrorColorTypeCellOrangePulse,
-    MirrorColorTypeCellYellow,
-    MirrorColorTypeCellYellowPulse,
-    MirrorColorTypeCellGreen,
-    MirrorColorTypeCellGreenPulse,
-    MirrorColorTypeCellTeal,
-    MirrorColorTypeCellTealPulse,
-    MirrorColorTypeCellBlue,
-    MirrorColorTypeCellBluePulse,
-    MirrorColorTypeCellPurple,
-    MirrorColorTypeCellPurplePulse,
-    MirrorColorTypeCellGray,
-    MirrorColorTypeCellGrayPulse,
 };
 
 @interface UIColor (MirrorColor)
