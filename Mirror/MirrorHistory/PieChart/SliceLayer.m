eafd68b4910b4b9d8f803a9d576738bda708d40c
wangyuqing.rachel <wangyuqing.rachel@gmail.com>
2023-05-03 13:59:35 +0800

    很细的百分比也可以点，也可以展示文案

--- Mirror/MirrorHistory/PieChart/SliceLayer.m
+++ Mirror/MirrorHistory/PieChart/SliceLayer.m
@@ -25,20 +25,18 @@
     if (selected) {
         CGFloat offset = self.pieWidth/20;
         newCenterPoint = CGPointMake(_centerPoint.x + cosf((_startAngle + _endAngle) / 2) * offset, _centerPoint.y + sinf((_startAngle + _endAngle) / 2) * offset);
-        if (_endAngle - _startAngle > 0.1) {
-            _textLayer = [CATextLayer layer];
-            CGFloat x = newCenterPoint.x + cosf((_startAngle + _endAngle)/2) * (self.pieWidth/3);
-            CGFloat y = newCenterPoint.y + sinf((_startAngle + _endAngle)/2) * (self.pieWidth/3);
-            CGFloat w = self.pieWidth/2;
-            CGFloat h = self.pieWidth/10;
-            _textLayer.frame = CGRectMake(x-w/2, y-h/2 , w, h);
-            _textLayer.alignmentMode = kCAAlignmentCenter;
-            _textLayer.string = self.text;
-            _textLayer.fontSize = h/2;
-            _textLayer.foregroundColor = [UIColor mirrorColorNamed:[UIColor mirror_getPulseColorType:self.colorType]].CGColor;
-            _textLayer.contentsScale = [UIScreen mainScreen].scale; // 解决catextlayer文字模糊的问题
-            [self.superlayer addSublayer:_textLayer]; // 添加在superlayer上，否则text会被slice切割
-        }
+        _textLayer = [CATextLayer layer];
+        CGFloat x = newCenterPoint.x + cosf((_startAngle + _endAngle)/2) * (self.pieWidth/3);
+        CGFloat y = newCenterPoint.y + sinf((_startAngle + _endAngle)/2) * (self.pieWidth/3);
+        CGFloat w = self.pieWidth/2;
+        CGFloat h = self.pieWidth/10;
+        _textLayer.frame = CGRectMake(x-w/2, y-h/2 , w, h);
+        _textLayer.alignmentMode = kCAAlignmentCenter;
+        _textLayer.string = self.text;
+        _textLayer.fontSize = h/2;
+        _textLayer.foregroundColor = [UIColor mirrorColorNamed:[UIColor mirror_getPulseColorType:self.colorType]].CGColor;
+        _textLayer.contentsScale = [UIScreen mainScreen].scale; // 解决catextlayer文字模糊的问题
+        [self.superlayer addSublayer:_textLayer]; // 添加在superlayer上，否则text会被slice切割
     }
     UIBezierPath *path = [UIBezierPath bezierPath];
     [path moveToPoint:newCenterPoint];
