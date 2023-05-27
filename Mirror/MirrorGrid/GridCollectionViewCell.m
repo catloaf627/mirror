aa36fed53754751928bdab025e9c56269ab67485
wangyuqing.rachel <wangyuqing.rachel@gmail.com>
2023-05-27 22:08:52 +0800

    alpha

--- Mirror/MirrorGrid/GridCollectionViewCell.m
+++ Mirror/MirrorGrid/GridCollectionViewCell.m
@@ -31,9 +31,15 @@
         }
         _totalTime = _totalTime + taskTime;
     }
-    if (_totalTime>0*3600) alpha = 0.5;
-    if (_totalTime>3*3600) alpha = 0.8;
-    if (_totalTime>7*3600) alpha = 1.0;
+    if (_totalTime>0*3600) alpha = 0.2; // 最浅为0.2，再浅就看不清了
+    if (_totalTime>1*3600) alpha = 0.3;
+    if (_totalTime>2*3600) alpha = 0.4;
+    if (_totalTime>3*3600) alpha = 0.5;
+    if (_totalTime>4*3600) alpha = 0.6;
+    if (_totalTime>5*3600) alpha = 0.7;
+    if (_totalTime>6*3600) alpha = 0.8;
+    if (_totalTime>7*3600) alpha = 0.9;
+    if (_totalTime>8*3600) alpha = 1.0; // 8小时以上即为最深色块
     
     if (_totalTime == 0) { // 无数据
         self.backgroundColor = [UIColor mirrorColorNamed:MirrorColorTypeAddTaskCellBG];
