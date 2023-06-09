8d6aa12bb078b355c10a69b7f4569da222894c5e
wangyuqing.rachel <wangyuqing.rachel@gmail.com>
2023-06-09 19:53:38 +0800

    comment

--- Mirror/MirrorSetups/main.m
+++ Mirror/MirrorSetups/main.m
@@ -5,6 +5,10 @@
 //  Created by Yuqing Wang on 2022/9/24.
 //
 
+// 上架App store流程链接
+// https://www.appcoda.com.tw/ios-app-submission/
+// https://blog.51cto.com/u_15848821/5823950
+
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
